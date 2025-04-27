f1a739295bfcd2e2386aa8ff417f3bea0d1afed0
merimcginnis <166745413+merimcginnis@users.noreply.github.com>
2025-04-27 22:39:59 +0000

    fixed error

--- botanaut-app/src/pages/Home.jsx
+++ botanaut-app/src/pages/Home.jsx
@@ -1,31 +1,41 @@
-<>
-  <Navbar />
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from '../components/Navbar';
 
-  <div className="homepage">
-    {/* HERO section with background */}
-    <div className="homepage-hero">
-      <Container>
-        <Row>
-          <Col>
-            <img className="HomeLogo" src="/images/logo.svg" alt="Home Logo" />
-          </Col>
-          <Col>
-            <img className="nova1" src="/images/nova-1.svg" alt="NOVA 1" />
-          </Col>
-        </Row>
-        <Row className="justify-content-center mt-4">
-          <div className="greeting-box">
-            <h2>Greetings, Cadet.<br />I’m NOVA, your Eco-Companion.</h2>
-          </div>
-        </Row>
-      </Container>
-    </div>
+export default function Home() {
+  return (
+    <>
+      <Navbar />
 
-    {/* Regular white content */}
-    <div className="homepage-content">
-      <Container>
-        {/* rest of your rows */}
-      </Container>
-    </div>
-  </div>
-</>
+      <div className="homepage">
+        {/* HERO section with background */}
+        <div className="homepage-hero">
+          <Container>
+            <Row>
+              <Col>
+                <img className="HomeLogo" src="/images/logo.svg" alt="Home Logo" />
+              </Col>
+              <Col>
+                <img className="nova1" src="/images/nova-1.svg" alt="NOVA 1" />
+              </Col>
+            </Row>
+            <Row className="justify-content-center mt-4">
+              <div className="greeting-box">
+                <h2>Greetings, Cadet.<br />I’m NOVA, your Eco-Companion.</h2>
+              </div>
+            </Row>
+          </Container>
+        </div>
+
+        {/* Regular white content */}
+        <div className="homepage-content">
+          <Container>
+            {/* rest of your rows */}
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+}
