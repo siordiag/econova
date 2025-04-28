aa17d24961bd6ea1e01c92e42cef820bd34d997c
merimcginnis <166745413+merimcginnis@users.noreply.github.com>
2025-04-28 01:26:38 +0000

    vite config

--- botanaut-app/vite.config.js
+++ botanaut-app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/botanaut-app/', // example: '/botanaut-app/' if that's the folder
 })
